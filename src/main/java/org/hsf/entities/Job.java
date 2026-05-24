67c4b84fc6be8034704d5dfc30a3e567e3cab4e9
duchuydt17 <88969009+duchuydt17@users.noreply.github.com>
2026-05-24 23:50:52 +0700

    feat: add job entity

---
+++ src/main/java/org/hsf/entities/Job.java
@@ -0,0 +1,49 @@
+package org.hsf.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "jobs")
+@NoArgsConstructor@AllArgsConstructor
+@Getter@Setter
+@ToString
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(500)")
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(500)")
+    private String location;
+
+    @Column(name = "salary_min", columnDefinition = "NUMERIC(15,2)")
+    private Double salaryMin;
+
+    @Column(name = "salary_max", columnDefinition = "NUMERIC(15,2)")
+    private Double salaryMax;
+
+    @Column(columnDefinition = "VARCHAR(50)")
+    private String status;
+
+    @Column(name = "utm_source", columnDefinition = "VARCHAR(150)")
+    private String utmSource;
+
+    @Column(name = "utm_medium", columnDefinition = "VARCHAR(150)")
+    private String utmMedium;
+
+    private OffsetDateTime deadline;
+
+    private OffsetDateTime published_at;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", referencedColumnName = "department_id")
+    private Department department;
+}
